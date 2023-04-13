c7eed96a8d2499f21e616eb4b0e841676de1f319
MaxiFranz <128651244+MaxiFranz@users.noreply.github.com>
2023-04-13 20:16:17 -0300

    corregimos ejercicio 2
    Reemplazo de archivos java, class y pdf

---
+++ Ejercicio N°2- Variables/ejercicio2Variables.java
@@ -0,0 +1,24 @@
+public class ejercicio2Variables {
+   
+	public static void main(String arg[]){
+       //declaramos las variables
+       int NumA = 10;
+       int NumB = 15;
+       int NumC;
+
+       //mostramos los valores iniciales
+       System.out.println("Los valores de las variables son:");
+       System.out.println("A =" +NumA );
+       System.out.println("B =" +NumB );
+       
+	//calculamos el valor de NumC segun operacion de consigna
+      
+	NumC = +NumA + +NumB;
+	
+	//imprimimos mensaje de resultado variable NumC
+	System.out.println("-------------------------------------");
+	System.out.println(" La suma de los variables es =" +NumC );
+	System.out.println("-------------------------------------");
+
+   } 
+}
