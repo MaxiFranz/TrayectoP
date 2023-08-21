125ba87c8428848c6118dca68b1afbf1b2151ee4
MaxiFranz <128651244+MaxiFranz@users.noreply.github.com>
2023-08-21 14:46:07 -0300

    Ejercicio 27poo

---
+++ NetBeansProyects/ejercicio27poook/src/ejercicio27poook/metodos.java
@@ -0,0 +1,26 @@
+
+package ejercicio27poook;
+
+
+public class metodos {
+    
+    //ventana 1 es la principal
+    //ventana 2 es si esta todo ok y no pide cambios
+    //ventana 3 es si no coincide la clave
+    //ventana 4 es si esta todo ok y pide cambios
+    
+    public static void fallo_login(int usuarioS, String n_usuarioS){
+        int usuarioSM = (usuarioS);
+        String n_usuarioSM = (n_usuarioS);
+        
+        //System.out.println("Metodo - Login incorrecto"); 
+        //System.out.println("Metodo - usuario "+n_usuarioSM); 
+        //System.out.println("Metodo - Numero usuario "+usuarioSM);    
+        
+        //ventana3 v3 = new ventana3();
+        //v3.setVisible(true);
+        
+    }
+    
+    
+}
